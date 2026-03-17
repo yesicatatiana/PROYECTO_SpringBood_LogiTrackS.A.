383e6ccb3b37a14c5e88c9d73c3c90ba20bc5685
yesica <ytatiana865@gmail.com>
2026-03-17 03:53:38 -0500

    feat: :sparkles: add

--- proyecto_spring/src/main/java/com/s1/proyecto_spring/config/SecurityConfig.java
+++ proyecto_spring/src/main/java/com/s1/proyecto_spring/config/SecurityConfig.java
@@ -78,7 +78,8 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowCredentials(false); // debe ser false cuando origins es "*"
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
